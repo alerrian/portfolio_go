9d41a91c6011386eee04956147d074c383ae7ce2
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-08 16:11:19 -0600

    Fix bug where html would not render on screen appropriately after dev.to

--- main.go
+++ main.go
@@ -19,9 +19,19 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static/"))
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
 
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index-dark.html", nil)
+	w.Header().Set("Content-Type", "text/html")
+
+	err := templates.ExecuteTemplate(w, `index.html`, nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
